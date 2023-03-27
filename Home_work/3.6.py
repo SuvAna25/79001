15ab9801791a6a97afdc6f90b7c6493eb45a5b2c
79001 <Barilova2220@mail.ru>
2023-03-27 15:21:08 +0300

    Решение задачи 3.6

---
+++ Home_work/3.6.py
@@ -0,0 +1,15 @@
+goods = [
+    ["яблоки", 50], ["апельсины", 190], ["груши", 100],
+    ["нектарины", 200], ["бананы", 77]
+]
+print('Текущий ассортимент: ', goods)
+
+fruit_name = input('Новый фрукт: ')
+fruit_price = int(input('Цена: '))
+goods.append([fruit_name, fruit_price])
+
+print('Новый ассортимент: ', goods)
+
+for good in goods:
+    good[1] = round(good[1] * 1.08 , 2)
+print('Новый ассортимент с увел. ценой: ', goods)
