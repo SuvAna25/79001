6ec0488380d7044d527e1e0d1aeb14d3db0e8116
79001 <Barilova2220@mail.ru>
2023-03-31 14:06:02 +0300

    Задача Магазин

--- Homework_on_module16/5task.py
+++ Homework_on_module16/5task.py
@@ -1,3 +1,7 @@
+#Напишите программу, которая генерирует новый список из первого
+#списка, заменяя все отрицательные числа на ноль.
+
 original_prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
 new_prices = [price if price > 0 else 0 for price in original_prices]
 print("Результат:\n", new_prices)
+
