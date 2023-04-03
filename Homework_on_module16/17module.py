114d0205a7c2f18d6c04ef19dcbddaf93c1a2b01
79001 <Barilova2220@mail.ru>
2023-04-03 12:17:27 +0300

    Задача решена

---
+++ Homework_on_module16/17module.py
@@ -0,0 +1,31 @@
+
+#задача 2
+#num = int(input('Введите длину списка:'))
+# num_list = [(1 if i % 2 == 0 else i % 5) for i in range(num)]
+#print(num_list)
+
+#задача 3
+import random
+
+#first_team = [round(random.uniform(5, 10),2) for _ in range(20)]
+#second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+#winners = [first_team if first_team > second_team else second_team]
+
+#print('Первая команда:', first_team, '\n', 'Вторая команда:', second_team)
+#print('Победители тура:', winners)
+
+#задача 4
+#alphabet = 'abcdefg'
+#new_list = alphabet[:]
+#print('1:', new_list)
+#print('2:', new_list[::-1])
+#print('3:', new_list[::2])
+#print('4:', new_list[1::2])
+#print('5:', new_list[:1])
+#print('6:', new_list[-1:-2:-1])
+#print('7:', new_list[3:4])
+#print('8:', new_list[-3:])
+#print('9:', new_list[3:5])
+#print('10:', new_list[4:2:-1])
+
+#задача 5
