718ca9b1423c2fc59b7a616f28ef2de617608cd8
79001 <Barilova2220@mail.ru>
2023-04-03 12:24:44 +0300

    Задача решена

--- Homework_on_module16/17module.py
+++ Homework_on_module16/17module.py
@@ -1,18 +1,2 @@
 
 
-
-#задача 4
-#alphabet = 'abcdefg'
-#new_list = alphabet[:]
-#print('1:', new_list)
-#print('2:', new_list[::-1])
-#print('3:', new_list[::2])
-#print('4:', new_list[1::2])
-#print('5:', new_list[:1])
-#print('6:', new_list[-1:-2:-1])
-#print('7:', new_list[3:4])
-#print('8:', new_list[-3:])
-#print('9:', new_list[3:5])
-#print('10:', new_list[4:2:-1])
-
-#задача 5
