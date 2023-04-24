379285774a9cab70af199a14fe4383f345ca52c9
79001 <Barilova2220@mail.ru>
2023-04-24 18:03:18 +0300

    Задача Склады

---
+++ Homework_on_module19/6.4.py
@@ -0,0 +1,22 @@
+small_storage = {
+    'гвозди': 5000,
+    'шурупы': 3040,
+    'саморезы': 2000
+}
+
+big_storage = {
+    'доски': 1000,
+    'балки': 150,
+    'рейки': 600
+}
+
+big_storage.update(small_storage)
+
+while True:
+    name = input('Введите название товара: ')
+    if name in big_storage:
+        print(big_storage.get(name))
+    else:
+        print('\nОшибка, такого товара нет.')
+
+
