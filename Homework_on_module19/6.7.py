e575cee1e20ee01331db83493fd1da9442f1f648
79001 <Barilova2220@mail.ru>
2023-04-26 16:57:10 +0300

    Задача Заказ фруктов

---
+++ Homework_on_module19/6.7.py
@@ -0,0 +1,23 @@
+order = {'apple': 2,
+         'banana': 3,
+         'pear': 1,
+         'watermelon': 10,
+         'chocolate': 5
+}
+
+incomes = {
+    'apple': 5600.20,
+    'orange': 3500.45,
+    'banana': 5000.00,
+    'bergamot': 3700.56,
+    'durian': 5987.23,
+    'grapefruit': 300.40,
+    'peach': 10000.50,
+    'pear': 1020.00,
+    'persimmon': 310.00,
+}
+result = 0
+for i_order in order:
+    num = incomes.get(i_order, 0) * order.get(i_order, 0)
+    result += num
+print('Итоговая сумма:', result)
