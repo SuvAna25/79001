574b5a85ccf59d58650483252684f5d9c571c42b
Ánastasiya <barilova2220@mail.ru>
2023-09-26 14:49:39 +0300

    all in one

---
+++ Homework_on_module22/9.5_os.py
@@ -0,0 +1,26 @@
+import os
+
+def find_file(cur_path, ending):
+    all_paths =[]
+    for i_elem in os.listdir(cur_path):
+        path = os.path.join(cur_path, i_elem)
+        if i_elem.endswith(ending):
+            all_paths.append(os.path.abspath(path))
+        elif os.path.isdir(path):
+            result = find_file(path, ending)
+            if result:
+                all_paths.extend(result)
+    return all_paths
+
+def get_text(path_to_file):
+    file = open(path_to_file, 'r', encoding='utf-8')
+    result = ''
+    for line in file:
+        result += line
+    return result
+
+all_file = find_file('..', '.py')
+file_result = open('skripts.txt', 'w', encoding='utf-8')
+for file_path in all_file:
+    file_result.write(get_text(file_path))
+    file_result.write('\n' * 2 + '*' * 80 + '\n' * 2)
